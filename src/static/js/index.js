5dee7e50779a4cc2c050d56c68bfe35d8cf3c3ba
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-12-06 02:49:28 +0100

    majosefa

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -19,6 +19,7 @@ import { palonoRender } from "./palono.js";
 
 import { gloriaRender } from "./gloria.js";
 import { glorinoRender } from "./glorino.js";
+import { majosefaRender } from "./majosefa.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
@@ -103,6 +104,8 @@ document.addEventListener('DOMContentLoaded', () => {
 
     glorinoRender();
 
+    majosefaRender();
+
 
     /* VIEWS */
 
@@ -116,6 +119,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let palonoView = document.getElementById("palono-view");
     let gloriaView = document.getElementById("gloria-view");
     let glorinoView = document.getElementById("glorino-view");
+    let majosefaView = document.getElementById("majosefa-view");
 
     let arr = []
 
@@ -127,6 +131,7 @@ document.addEventListener('DOMContentLoaded', () => {
     arr.push(palonoView);
     arr.push(gloriaView);
     arr.push(glorinoView);
+    arr.push(majosefaView);
 
     
 
@@ -149,6 +154,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const palonoLink = document.querySelector('a[href="#palono-view"]');
     const gloriaLink = document.querySelector('a[href="#gloria-view"]');
     const glorinoLink = document.querySelector('a[href="#glorino-view"]');
+    const majosefaLink = document.querySelector('a[href="#majosefa-view"]');
 
     studentListLink.addEventListener("click", () => showView(studentView));
     addTeacherLink.addEventListener("click", () => showView(teacherView));
@@ -159,6 +165,7 @@ document.addEventListener('DOMContentLoaded', () => {
     palonoLink.addEventListener("click", () => showView(palonoView));
     gloriaLink.addEventListener("click", () => showView(gloriaView));
     glorinoLink.addEventListener("click", () => showView(glorinoView));
+    majosefaLink.addEventListener("click", () => showView(majosefaView));
 
     // Show the student list view by default
     showView(presentacionView);
---
+++ src/static/js/majosefa.js
@@ -0,0 +1,166 @@
+const majosefaForm = document.getElementById("majosefa-form");
+const editMajosefaForm = document.getElementById("editMajosefa-form");
+const majosefaList = document.getElementById("majosefa-list");
+
+const majosefaAdd = document.getElementById("addMajosefa-form");
+const majosefaEdit = document.getElementById("editMajosefa-form");
+const editMajosefaTitle = document.getElementById("editMajosefa-title");
+
+const majosefaAddBtn = document.getElementById("addMajosefa-btn");
+
+export function createMajosefaListItem(name, id) {
+    const listItem = document.createElement("li");
+    listItem.className = "list-group-item d-flex justify-content-between align-items-center";
+
+    listItem.setAttribute('customId', id);
+    listItem.id = `majosefa-li-${id}`;
+    const majosefaInfo = document.createElement("span");
+    majosefaInfo.textContent = `${name}`;
+
+    const buttonsDiv = document.createElement("div");
+
+    const updateButton = document.createElement("button");
+    // updateButton.textContent = "Update";
+    const updateIcon = document.createElement("i");
+    updateIcon.className = "bi bi-pencil-square"; // Bootstrap trash icon
+    updateButton.appendChild(updateIcon);
+    updateButton.className = "btn btn-success mr-2";
+
+    const removeButton = document.createElement("button");
+    const removeIcon = document.createElement("i");
+    removeIcon.className = "bi bi-trash"; // Bootstrap trash icon
+    removeButton.appendChild(removeIcon);
+    removeButton.className = "btn btn-danger";
+
+    // removeButton.className = "btn btn-danger";
+
+    updateButton.addEventListener("click", () => {
+
+        displayEditMajosefa(majosefaInfo.textContent, id);
+    });
+
+    removeButton.addEventListener("click", () => {
+        const id = listItem.getAttribute('customId');
+        listItem.remove();
+        fetch(`http://localhost/majosefas/${id}`, {
+            method: "DELETE"
+        })
+            .then(response => response.json())
+            .then(datos => console.log(datos));
+    });
+
+    buttonsDiv.appendChild(updateButton);
+    buttonsDiv.appendChild(removeButton);
+
+    listItem.appendChild(majosefaInfo);
+    listItem.appendChild(buttonsDiv);
+
+    return listItem;
+}
+
+export function displayEditMajosefa(name, id) {
+
+    editMajosefaTitle.setAttribute("customId", id);
+    editMajosefaTitle.textContent = `Edit majosefa ${name}`;
+
+    editMajosefaForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const name = document.getElementById("name-majosefa-edit").value;
+
+
+        const data = {
+            name,
+        }
+
+        fetch(`http://localhost/majosefas/${id}`, {
+            method: "PUT",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json"
+            },
+        })
+            .then(response => response.json())
+            .then(datos => {
+                console.log(datos)
+                console.log("PUTTT")
+                console.log(`datos.id = ${datos.id}`)
+                const listItem = createMajosefaListItem(name, age, datos.id);
+                majosefaList.appendChild(listItem);
+                majosefaForm.reset();
+            });
+
+        document.getElementById(`majosefa-li-${id}`).remove()
+    });
+
+
+    majosefaAdd.style.display = "none";
+    majosefaEdit.style.display = "block";
+}
+
+export function displayAddMajosefa() {
+    majosefaAdd.style.display = "block";
+    majosefaEdit.style.display = "none";
+}
+
+
+
+export function majosefaRender() {
+
+    /* TEACHER */
+
+
+    const majosefaForm = document.getElementById("majosefa-form");
+    const majosefaList = document.getElementById("majosefa-list");
+
+    const majosefas = fetch("http://localhost/majosefas/", {
+        method: "GET"
+    }).then(response => response.json())
+        .then(data => {
+
+            // Loop through each item in the array
+            data.forEach(item => {
+                // Do something with each item
+                console.log(item);
+                console.log("array")
+
+                const listItem = createMajosefaListItem(item.name, item.id);
+                majosefaList.appendChild(listItem);
+                majosefaForm.reset();
+
+            });
+        })
+
+    console.log("olaa2222")
+
+    majosefaForm.addEventListener("submit", function (e) {
+        e.preventDefault();
+        const name = document.getElementById("name-majosefa").value;
+
+
+
+        const data = {
+            name
+        }
+
+        fetch("http://localhost/majosefas/", {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                "Content-type": "application/json"
+            },
+        })
+            .then(response => response.json())
+            .then(datos => {
+                console.log(datos)
+                console.log("POSTTT")
+                console.log(`datos.id = ${datos.id}`)
+                const listItem = createMajosefaListItem(name, datos.id);
+                majosefaList.appendChild(listItem);
+                majosefaForm.reset();
+            });
+    });
+
+
+    const majosefaAddBtn = document.getElementById("addMajosefa-btn");
+    majosefaAddBtn.addEventListener("click", displayAddMajosefa);
+}
