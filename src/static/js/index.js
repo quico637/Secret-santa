b6a8be4863465cd1637cca70c78de19e9dc2707d
Francisco Arnaldo Boix Martinez <96247160+quico637@users.noreply.github.com>
2023-12-06 03:28:21 +0100

    permissions

--- src/static/js/index.js
+++ src/static/js/index.js
@@ -26,71 +26,7 @@ import { merfonoRender } from "./merfono.js";
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    /* STUDENT */
-
-    // JavaScript code to handle form submission
-    const studentForm = document.getElementById("student-form");
-    const studentList = document.getElementById("student-list");
-
-
-    const students = fetch("http://localhost/students/", {
-        method: "GET"
-    }).then(response => response.json())
-        .then(data => {
-
-            // Loop through each item in the array
-            data.forEach(item => {
-                // Do something with each item
-                console.log(item);
-                console.log("array")
-
-                const listItem = createStudentListItem(item.name, item.age, item.id);
-                studentList.appendChild(listItem);
-                studentForm.reset();
-
-            });
-        })
-
-
-    studentForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const name = document.getElementById("name").value;
-        const age = document.getElementById("age").value;
-        const subject = document.getElementById("subject").value;
-        const group = document.getElementById("groupSelector-student").value;
-
-
-        const data = {
-            name,
-            age,
-            subject,
-            group
-        }
-
-        fetch("http://localhost/students/", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-type": "application/json"
-            },
-        })
-            .then(response => response.json())
-            .then(datos => {
-                console.log(datos)
-                console.log("POSTTT")
-                console.log(`datos.id = ${datos.id}`)
-                const listItem = createStudentListItem(name, age, datos.id);
-                studentList.appendChild(listItem);
-                studentForm.reset();
-            });
-
-
-    });
-
-
-    /* TEACHER */
-    console.log("olaa")
-    teacherRender();
+   
 
 
     /* PALOMA */
@@ -120,9 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     // JavaScript code to switch between views
-    let teacherView = document.getElementById("teacher-view");
-    let studentView = document.getElementById("student-view");
-    let agendaView = document.getElementById("agenda-view");
+
     let presentacionView = document.getElementById("presentacion-view");
     let palomaView = document.getElementById("paloma-view");
     let palonoView = document.getElementById("palono-view");
@@ -135,9 +69,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     let arr = []
 
-    arr.push(teacherView);
-    arr.push(studentView);
-    arr.push(agendaView);
     arr.push(presentacionView);
     arr.push(palomaView);
     arr.push(palonoView);
@@ -186,77 +117,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Show the student list view by default
     showView(presentacionView);
 
-
-
-
-
-
-    // inside student's view
-
-    const studentAddBtn = document.getElementById("addStudent-btn");
-    studentAddBtn.addEventListener("click", displayAddStudent);
-
-
-    /* CALENDAR */
-
-    calendar.setUp();
-
-
-    /* MEETING */
-
-    const submitMettingButton = document.getElementById("meetingSubmitButton");
-
-    submitMettingButton.addEventListener("click", function (e) {
-
-        e.preventDefault();
-        const startHour = document.getElementById("startHour").value;
-        const endHour = document.getElementById("endHour").value;
-        const interval = document.getElementById("interval").value;
-        const studentsPerInterval = document.getElementById("studentsPerInterval").value;
-        const meetingDate = document.getElementById("meetingDate").value;
-        const groupSelector = document.getElementById("groupSelector");
-        const subject = document.getElementById("subject").value;
-        const teacher = document.getElementById("teacher").value;
-        const location = document.getElementById("location").value;
-        const meetingName = document.getElementById("meetingName").value;
-
-        const meetingForm = document.getElementById("meetingForm");
-
-        const data = {
-            startHour,
-            endHour,
-            interval,
-            studentsPerInterval,
-            meetingDate,
-            group: groupSelector.value,
-            subject,
-            teacher,
-            location,
-            meetingName
-        }
-
-        fetch("http://localhost/meetings/", {
-            method: "POST",
-            body: JSON.stringify(data),
-            headers: {
-                "Content-type": "application/json"
-            },
-        })
-            .then(response => response.json())
-            .then(datos => {
-                console.log(datos)
-                // const listItem = createMeetingListItem(meetingName, startHour, endHour, datos.id);
-                // meetingList.appendChild(listItem);
-                meetingForm.reset();
-            });
-
-    });
-
-
-    // inside meeting view 
-
-    const meetingAddBtn = document.getElementById("addMeeting-btn");
-    meetingAddBtn.addEventListener("click", displayAddMeeting);
 
 });
 
